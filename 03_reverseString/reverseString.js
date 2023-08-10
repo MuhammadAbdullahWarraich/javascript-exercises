24e86b24fb928e700eac4de54a7b6eb66ba62b60
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-08-10 14:46:27 +0500

    fix: solve exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let answer = '';
+  const length = string.length;
+  for (let i = length - 1; i >= 0; i--)  {
+    answer += string[i]; 
+  }
+  return answer;
 };
 
 // Do not edit below this line
