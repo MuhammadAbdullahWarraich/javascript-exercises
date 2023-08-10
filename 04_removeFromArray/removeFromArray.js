ba08a99a3eb5f0be3ef91d7d6a03daf93f449b21
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-08-10 15:50:34 +0500

    feat: add another solution to exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,19 @@
-const removeFromArray = function(arr, ...removeUs) {
+// const removeFromArray = function(arr, ...removeUs) {
+//   let answer = [];
+//   arrIndex: for (element of arr)  {
+//     for (let i = 0; i < removeUs.length; i++) {
+//       if (element === removeUs[i]) continue arrIndex;
+//     }
+//     answer.push(element);
+//   }
+//   return answer;
+// };
+
+const removeFromArray = function(arr, elementsToBeRemoved) {
   let answer = [];
   arrIndex: for (element of arr)  {
-    for (let i = 0; i < removeUs.length; i++) {
-      if (element === removeUs[i]) continue arrIndex;
+    for (let i = 1; i < arguments.length; i++) {
+      if (element === arguments[i]) continue arrIndex;
     }
     answer.push(element);
   }
