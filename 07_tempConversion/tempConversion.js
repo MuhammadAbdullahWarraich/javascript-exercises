61ddafa36481cb680a92a52d02aef33af193b5e2
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-08-10 19:55:03 +0500

    fix: solve exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let answer = ((fahrenheitTemp - 32) * 5 / 9) * 10;
+  answer = Math.round(answer)/10;
+  return answer;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let answer = ((celsiusTemp * 9 / 5) + 32) * 10;
+  answer = Math.round(answer)/10;
+  return answer;
 };
 
 // Do not edit below this line
