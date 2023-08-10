f8b8fe06a9d4d6a468ae322a8a8bd8dfc71dc7ef
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-08-10 16:17:20 +0500

    fix: solve exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let answer = 0;
+  if ((num1 < 0) || (num2 < 0) || ((typeof num1 !== "number") || (typeof num2 !== "number"))) return 'ERROR';
 
+  const lowerRange = (num1 < num2)?num1:num2;
+  const upperRange = (num1 < num2)?num2:num1;
+
+  for (let i = lowerRange; i <= upperRange; i++)  {
+    answer += i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
