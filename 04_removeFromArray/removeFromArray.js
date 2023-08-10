81517d7f9625684441580a7aac3611408378c352
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-08-10 15:35:46 +0500

    fix: solve exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...removeUs) {
+  let answer = [];
+  arrIndex: for (element of arr)  {
+    for (let i = 0; i < removeUs.length; i++) {
+      if (element === removeUs[i]) continue arrIndex;
+    }
+    answer.push(element);
+  }
+  return answer;
 };
 
 // Do not edit below this line
